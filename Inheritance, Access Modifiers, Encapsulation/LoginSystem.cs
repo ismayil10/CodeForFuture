e3fde1fdfb7a408cc0c7741e5fea9550978b2afd
ismayil10 <ismayiltg-apa101@code.edu.az>
2025-10-16 21:12:19 +0400

    Add files via upload

---
+++ Inheritance, Access Modifiers, Encapsulation/LoginSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance__Access_Modifiers__Encapsulation
+{
+    internal class LoginSystem
+    {
+    }
+}
