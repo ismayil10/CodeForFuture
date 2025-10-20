2430f14257bc5e657f9cf8c9880c1587f8923b1e
ismayil10 <ismayiltg-apa101@code.edu.az>
2025-10-20 21:27:41 +0400

    Add files via upload

---
+++ Static, Extentions/IntExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace public_static_class_IntExtensions
+{
+    public static class IntExtensions
+    {
+        public static int Pow(this int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
---
+++ Static, Extentions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace public_static_class_IntExtensions
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            Console.WriteLine(a.Pow(4)); 
+        }
+    }
+}
+
