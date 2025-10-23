2dcec35552d02b08078441e4abee7995c3f04a47
ismayil10 <ismayiltg-apa101@code.edu.az>
2025-10-23 21:42:50 +0400

    Add files via upload

---
+++ Downcasting, Implicit-Explicit (Operator overloading)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Kelvin
+{
+    public double Degree { get; set; }
+
+    public Kelvin(double degree)
+    {
+        Degree = degree;
+    }
+}
+
+public class Celsius
+{
+    public double Degree { get; set; }
+
+    public Celsius(double degree)
+    {
+        Degree = degree;
+    }
+
+    public static implicit operator Kelvin(Celsius c)
+    {
+        return new Kelvin(c.Degree + 273);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Celsius c = new Celsius(0);
+        Kelvin k = c; 
+
+        Console.WriteLine($"Celsius: {c.Degree}°C");
+        Console.WriteLine($"Kelvin: {k.Degree}K");
+    }
+}
