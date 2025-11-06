ffb922a1298e89ccc883cfeb568dcdb3fa7d91aa
ismayil10 <ismayiltg-apa101@code.edu.az>
2025-11-06 22:37:55 +0400

    Add files via upload

---
+++ LibraryApp/Repository/Repositories/Interface/IRepositories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories.Interface
+{
+    public interface IRepositories<T>
+    {
+        void Creare(T data);
+        void Delete(int Id);
+        List<T> GetAll(Predicate<T>? predicateq);
+        T Get(Predicate<T> predicate);
+        void Update(T data, int id);
+
+    }
+}
